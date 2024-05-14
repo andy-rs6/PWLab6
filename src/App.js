93dcbf3d55b993d13b94ed3aee67634596818133
Andy <apple@Andy-MacBook-Pro.local>
2024-05-14 14:32:13 +0300

    save and retrieve to local store

--- src/App.js
+++ src/App.js
@@ -27,10 +27,21 @@ const App = () => {
     getMovieRequest(searchValue);
   }, [searchValue]);
 
+  useEffect(() => {
+    const movieFavourites = JSON.parse(localStorage.getItem('react-movie-app-favourites'));
+
+    setFavourites(movieFavourites);
+  }, []);
+
+  const saveToLocalStorage = (items) => {
+    localStorage.setItem('react-movie-app-favourites', JSON.stringify(items));
+  };
+
   const addFavouriteMovie = (movie) => {
     const newFavouriteList = [...favourites, movie];
     setFavourites(newFavouriteList);
-  }
+    saveToLocalStorage(newFavouriteList);
+  };
 
 	const removeFavouriteMovie = (movie) => {
 		const newFavouriteList = favourites.filter(
@@ -38,6 +49,7 @@ const App = () => {
 		);
 
 		setFavourites(newFavouriteList);
+    saveToLocalStorage(newFavouriteList);
 	};
 
   return (
